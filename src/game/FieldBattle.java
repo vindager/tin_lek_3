6a60688d84174c00a431fe26f543532749cdfd1c
vindager <vindager.prod@yandex.ru>
2020-02-25 21:11:35 +0300

    ver 1.11
    ver 1.11

--- src/game/FieldBattle.java
+++ src/game/FieldBattle.java
@@ -7,6 +7,9 @@ public class FieldBattle {
     private String[][] field;
     private int sizeField = 10;
 
+   new char[] {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
+
+
     public FieldBattle() {
         field = new String[sizeField][sizeField];
         setWawe();
@@ -17,8 +20,8 @@ public class FieldBattle {
      */
     private void setWawe() {
         field[0][0] = " ";
-        for (int x = 0; x < sizeField; x++) {
-            for (int y = 0; y < sizeField; y++) {
+        for (int x = 0; x < sizeField; ++x) {
+            for (int y = 0; y < sizeField; ++y) {
                 field[x][y] = "~";
             }
         }
@@ -28,11 +31,28 @@ public class FieldBattle {
      * Вывод поля в консоль.
      */
     public void printField() {
-        for (int x = 0; x < sizeField; x++) {
-            for (int y = 0; y < sizeField; y++) {
+
+        int coordX = 1;
+        int coordY = 1;
+
+        System.out.print( ' ' );
+        for(int x = 0; x < sizeField; ++x) {
+
+            System.out.print(' ');
+            System.out.print(coordX++);
+        }
+        System.out.println();
+
+        for (int y = 0; y < sizeField; ++y) {
+            System.out.print( coordY++ );
+
+            for (int x = 0; x < sizeField; ++x) {
+                if( !(x == 0 && coordY == 11) ) {
+                    System.out.print( ' ' );
+                }
                 System.out.print(field[x][y]);
             }
-            System.out.println("");
+            System.out.println("|");
         }
     }
 }
